426ca4011423e1519829c636c8898a8cb58e8f02
Admin <admin@Admins-MacBook-Pro.local>
2020-06-01 14:27:34 +0530

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express=require('express');
 const app=express();
+app.set("view engine","ejs");
+app.set("views","./viewss")
 
 app.get("/",function(req,res){
-res.sendFile(__dirname+"/views/login.html")
+    res.render("login")
 
 })
 app.get("/home",function(req,res){
-    res.sendFile(__dirname+"/views/home.html")
+    res.render("home")
 })
 
 app.post("/login",function(req,res){
